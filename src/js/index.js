e2a568c295603bb96cb1e2357b220d6e146ff431
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-09-25 12:27:52 -0700

    simplifying file structure

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Example from './template.js';
+import Example from './coinCounter.js';
 
 
 $(document).ready(function() {
