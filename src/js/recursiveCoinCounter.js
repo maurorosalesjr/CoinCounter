a8683f50ebc952e4057b98f922254ed0a644f192
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-09-26 18:37:58 -0700

    updated the console.log to properly document math rounding error

--- src/js/recursiveCoinCounter.js
+++ src/js/recursiveCoinCounter.js
@@ -4,19 +4,19 @@
 function recursiveCoinCounter(amount){
 if(amount >= .25){
   quarters = Math.floor(amount/.25);
-  console.log(amount/.25);
+  console.log(amount);
   return (`${quarters} Quarters, ` + recursiveCoinCounter(amount - (quarters * .25)));
 } if(amount >= .1){
   dimes = Math.floor(amount/.1);
-  console.log(amount/.1);
+  console.log(amount);
   return (`${dimes} Dimes, ` + recursiveCoinCounter(amount - (dimes * .1)));
 } if(amount >= .05){
   nickles = Math.floor(amount/.05);
-  console.log(amount/.05);
+  console.log(amount);
   return (`${nickles} Nickles, ` + recursiveCoinCounter(amount - (nickles * .05)));
 } else {
   pennies = Math.floor(amount/.01);
-  console.log(amount/.01);
+  console.log(amount);
   return (`${pennies} Pennies`);
 }
 }
