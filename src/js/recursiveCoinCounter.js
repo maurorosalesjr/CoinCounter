a83584e152ccb993c019ccdda44fa500735e9950
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-09-28 18:51:02 -0700

    fixed rounding error

--- src/js/recursiveCoinCounter.js
+++ src/js/recursiveCoinCounter.js
@@ -5,19 +5,18 @@ function recursiveCoinCounter(amount){
 if(amount >= .25){
   quarters = Math.floor(amount/.25);
   console.log(amount);
-  return (`${quarters} Quarters, ` + recursiveCoinCounter(amount - (quarters * .25)));
+  return (`${quarters} Quarters, ` + recursiveCoinCounter((amount - (quarters * .25)).toFixed(2)));
 } if(amount >= .1){
   dimes = Math.floor(amount/.1);
   console.log(amount);
-  return (`${dimes} Dimes, ` + recursiveCoinCounter(amount - (dimes * .1)));
+  return (`${dimes} Dimes, ` + recursiveCoinCounter((amount - (dimes * .1)).toFixed(2)));
 } if(amount >= .05){
   nickles = Math.floor(amount/.05);
   console.log(amount);
-  return (`${nickles} Nickles, ` + recursiveCoinCounter(amount - (nickles * .05)));
+  return (`${nickles} Nickles, ` + recursiveCoinCounter((amount - (nickles * .05)).toFixed(2)));
 } else {
   pennies = Math.floor(amount/.01);
   console.log(amount);
   return (`${pennies} Pennies`);
 }
 }
-// there is a rounding error i can't fix at this moment
